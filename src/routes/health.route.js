01adcaf801b42b4e1483263b3df1ad2341b2c495
Zankhana <er.zankhana94@gmail.com>
2026-05-18 18:58:37 +0530

    message updated

--- src/routes/health.route.js
+++ src/routes/health.route.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.json({
     success: true,
-    message: "CI/CD Working.......",
+    message: "CI/CD Working...........",
     timestamp: new Date(),
   });
 });
